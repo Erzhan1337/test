e718293fd5a40272dfe3e749a391c179131b9dec
Erzhan <code.ninja.erzhan@gmail.com>
2026-05-21 00:24:32 +0500

    base conf axios

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -5,9 +5,19 @@ import { useState } from "react";
 export default function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await 
+    }
+  }
 
   return (
-    <form className="w-100 border">
+    <form className="w-100 border" onSubmit={handleSubmit}>
       <div className="flex flex-col items-center p-5">
         <p className="text-xl mb-5">Login</p>
         <div className="w-full flex flex-col gap-5">
@@ -28,6 +38,7 @@ export default function LoginForm() {
         </div>
         <button
           type="submit"
+          disabled={isLoading}
           className="bg-blue-500 text-white p-2 rounded-xl mt-5 w-full cursor-pointer"
         >
           Login
